2c237db14d4dda078b936afeafd564da4ac97912
+ <+>
2023-10-01 14:48:49 +0300

    all test passing

--- lib/dog.py
+++ lib/dog.py
@@ -1,42 +1,31 @@
+import os
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from models import Dog
 
-engine = create_engine('sqlite:///:memory:')
 
-def create_table(base):
-    sql = """
-        CREATE TABLE IF NOT EXISTS dogs (
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            breed TEXT
-        );
-    """
-
-    return engine
+def create_table(base, create_engine):
+    base.metadata.create_all(create_engine)
 
 
 def save(session, dog):
     session.add(dog)
     session.commit()
-
-    return session
-
-def new_from_db(session, row):
-    return session.query(Dog).filter_by(id = row.id).first()
-
+    
 def get_all(session):
-    return session.query(Dog)
+    return session.query(Dog).all()
+
 
 def find_by_name(session, name):
-    return session.query(Dog).filter_by(name = name).first()
+    return session.query(Dog).filter_by(name=name).first()
 
 def find_by_id(session, id):
-    return session.query(Dog).filter_by(id = id).first()
+    return session.query(Dog).filter_by(id=id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    return session.query(Dog).filter_by(name = name, breed = breed).first()
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
 
-def update_breed(session, dog, breed):
-    dog.breed = breed
+def update_breed(session, dog, new_breed):
+    dog.breed = new_breed
     session.commit()
-    return session
+    
